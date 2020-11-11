4ef9716f74954eb5de65e3fdbfeb70a8496b58d0
khoironi2 <khoironi7887@students.amikom.ac.id>
2020-11-11 15:45:18 +0700

    database baru

--- sikpkbp.sql
+++ sikpkbp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 10, 2020 at 04:57 PM
+-- Generation Time: Nov 11, 2020 at 03:44 PM
 -- Server version: 5.7.24
 -- PHP Version: 5.6.37
 
@@ -56,6 +56,21 @@ CREATE TABLE `tbl_donasi` (
   `created_by` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `tbl_donasi`
+--
+
+INSERT INTO `tbl_donasi` (`id_donasi`, `id_users`, `nominal_donasi`, `bukti_donasi`, `validasi_donasi`, `time_create_donasi`, `time_update_donasi`, `id_kegiatan`, `created_by`) VALUES
+(1, 2, '350000', 'ok', 'sudah_tranfer', '2020-11-10 22:36:20', '2020-11-10 22:36:20', 1, NULL),
+(2, 1, '800000', 'ok', 'sudah_tranfer', '2020-11-10 23:01:19', '2020-11-10 23:01:19', 2, NULL),
+(3, NULL, '1000', NULL, 'sudah_tranfer', '2020-11-10 23:28:15', '2020-11-10 23:28:15', 3, 2),
+(4, NULL, '1000', NULL, 'sudah_tranfer', '2020-11-10 23:30:40', '2020-11-10 23:30:40', 4, 2),
+(5, 1, '2000000', 'ok', 'sudah_tranfer', '2020-11-10 23:39:42', '2020-11-10 23:39:42', 1, NULL),
+(6, 1, '1500000', 'ok', 'sudah_tranfer', '2020-11-10 23:40:37', '2020-11-10 23:40:37', 1, NULL),
+(7, 1, '900000', 'IMG-20180720-WA00061.jpg', 'sudah_tranfer', '2020-11-11 10:23:38', '2020-11-11 13:31:43', 3, NULL),
+(8, 1, '90000000', NULL, 'belum_transfer', '2020-11-11 10:42:30', '2020-11-11 13:12:47', 2, NULL),
+(9, 1, '90000000', 'IMG-20180720-WA0006.jpg', 'terkonfirmasi', '2020-11-11 10:59:35', '2020-11-11 13:28:35', 2, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -71,8 +86,19 @@ CREATE TABLE `tbl_kegiatan` (
   `nominal_dana_kegiatan` varchar(255) DEFAULT NULL,
   `time_create_kegiatan` datetime DEFAULT NULL,
   `time_update_kegiatan` datetime DEFAULT NULL,
-  `id_users` int(11) DEFAULT NULL
+  `id_users` int(11) DEFAULT NULL,
+  `status_kegiatan` enum('aktif','belum_aktif') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_kegiatan`
+--
+
+INSERT INTO `tbl_kegiatan` (`id_kegiatan`, `nama_kegiatan`, `time_pelakasanaan_kegiatan`, `gambar_kegiatan`, `deskripsi_kegitan`, `nominal_dana_kegiatan`, `time_create_kegiatan`, `time_update_kegiatan`, `id_users`, `status_kegiatan`) VALUES
+(1, 'Sedekah Jalanan', '2020-11-20 00:00:00', 'uas.jpg', NULL, '3500000', NULL, NULL, NULL, 'aktif'),
+(2, 'Kegiatan Amal', '2020-11-27 00:00:00', 'download.jpg', NULL, '8700000', NULL, NULL, NULL, 'aktif'),
+(3, 'Mancing Mania', '2020-11-30 00:00:00', 'kapal_pniisi.jpg', NULL, '8400000', NULL, NULL, NULL, 'aktif'),
+(4, 'Galang Bantuan Covid - 9', '2020-12-12 00:00:00', 'Foto-Penyerahan.jpg', NULL, '178600000', NULL, NULL, NULL, 'aktif');
 
 -- --------------------------------------------------------
 
@@ -115,9 +141,9 @@ CREATE TABLE `tbl_users` (
 --
 
 INSERT INTO `tbl_users` (`id_users`, `name`, `telepon_users`, `email`, `password`, `level`, `time_login_users`, `time_logout_users`, `gambar_users`, `time_create_users`, `time_update_users`) VALUES
-(1, 'khoironi', '085156587695', 'masrony37@gmail.com', '$2y$10$Iz1BhaHXYF8OMGkFUlPt3.TPB/W5bIP5i4AXDDERz/lNzJHF1rwR6', 'donatur', NULL, NULL, NULL, '2020-11-10 16:51:42', NULL),
-(2, 'adeline', '0895634369096', 'akusaraadeline20@gmail.com', '$2y$10$.b3R/e1R/oUrpMCduMBJ0eJfdZygo/bzIYOZ/AAaBx73uGBOCBcZi', 'admin', NULL, NULL, NULL, '2020-11-10 16:53:33', NULL),
-(3, 'ketua', '08512387434', 'ketua.adeline@gmail.com', '$2y$10$ixPVNjOqQCJmlaKqWb71IuhbE7IDDI7J1WDRKP4o6YgZpEogS8dT6', 'ketua', NULL, NULL, NULL, '2020-11-10 16:55:34', NULL);
+(1, 'khoironi', '085156587695', 'masrony37@gmail.com', '$2y$10$Iz1BhaHXYF8OMGkFUlPt3.TPB/W5bIP5i4AXDDERz/lNzJHF1rwR6', 'donatur', '2020-11-11 15:35:20', '2020-11-11 15:38:48', NULL, '2020-11-10 16:51:42', NULL),
+(2, 'adeline', '0895634369096', 'akusaraadeline20@gmail.com', '$2y$10$.b3R/e1R/oUrpMCduMBJ0eJfdZygo/bzIYOZ/AAaBx73uGBOCBcZi', 'admin', '2020-11-10 23:07:14', '2020-11-10 23:35:37', NULL, '2020-11-10 16:53:33', NULL),
+(3, 'ketua', '08512387434', 'ketua.adeline@gmail.com', '$2y$10$ixPVNjOqQCJmlaKqWb71IuhbE7IDDI7J1WDRKP4o6YgZpEogS8dT6', 'ketua', '2020-11-10 20:59:15', '2020-11-10 21:07:08', NULL, '2020-11-10 16:55:34', NULL);
 
 --
 -- Indexes for dumped tables
@@ -167,13 +193,13 @@ ALTER TABLE `tbl_dokumentasi`
 -- AUTO_INCREMENT for table `tbl_donasi`
 --
 ALTER TABLE `tbl_donasi`
-  MODIFY `id_donasi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_donasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tbl_kegiatan`
 --
 ALTER TABLE `tbl_kegiatan`
-  MODIFY `id_kegiatan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kegiatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_laporan_donasi`
