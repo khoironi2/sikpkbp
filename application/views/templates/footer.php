9472d0dff46c04fe7fa1d2a225f89f7a6c87b2cb
khoironi2 <khoironi7887@students.amikom.ac.id>
2020-11-12 08:56:58 +0700

    footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,10 @@
+    <footer>
+        <div class="container mt-3">
+            <div class="row justify-content-center">
+                <p style="font-size: 12px;">Copyright <a href="<?= base_url('') ?>">SIKPKBP</a> 2020</p>
+            </div>
+        </div>
+    </footer>
     <script src="<?= base_url('assets/js/jquery-3.5.1.min.js') ?>"></script>
     <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
 
